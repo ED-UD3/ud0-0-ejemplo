6e6fee966cd07ad6619652cedba0f20da466f255
r.palaolozano <128861590+rpalaolozano@users.noreply.github.com>
2024-06-27 12:16:57 +0200

    Update functions.php

--- src/utils/functions.php
+++ src/utils/functions.php
@@ -1,6 +1,10 @@
 <?php
     //Función para poder probar la aplicación en el navagador. No participa en el los tests
     function principal() {
-        
+        printMessage();
+    }
+
+    function printMessage{
+        echo "Hello, world";
     }
 ?>
